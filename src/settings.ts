5cc7d0cd29733b4ccf90f26da4be829d9533ff1b
Josh Attoun <josh.attoun@sviworld.com>
2025-06-30 13:10:48 -0500

    point to railway

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,3 @@
 // src/settings.ts
 
-export const CHAT_SERVER_URL = import.meta.env.VITE_SMKT_CHAT_SERVER_URL as string;
+export const CHAT_SERVER_URL = import.meta.env.VITE_PYTHON_SERVER_URL as string;
