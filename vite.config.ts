b8ba8d89e6cc1c6e67d9ca80562e9b30dcf6f3bb
Josh Attoun <josh.attoun@sviworld.com>
2025-06-30 07:59:37 -0500

    relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     root: './',
+    publicDir: './public',
     server: {
         host: "::",
         port: 8080,
 
     },
     plugins: [
-        react(),
-        //mode === 'development' 
+        react(),        
     ].filter(Boolean),
     resolve: {
         alias: {
