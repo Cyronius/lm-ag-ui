51f96aab888775e5e1c736ff48b6cec391ab21f8
Josh Attoun <josh.attoun@sviworld.com>
2025-07-11 17:04:10 -0500

    first round of ag-ui support using copilotkit

--- src/components/ChatMessages.tsx
+++ src/components/ChatMessages.tsx
@@ -1,50 +1,75 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import { Typography, CircularProgress } from '@mui/material';
 import { Bot, User } from 'lucide-react';
 import './ChatInterface.css';
 import './ChatMessages.css';
+import { AgentStateMessage, ImageMessage, Message, MessageRole, TextMessage, TextMessageInput } from '@copilotkit/runtime-client-gql';
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { ChatEvent, Content } from '../types';
+import { useCopilotChat } from '@copilotkit/react-core';
 
-interface ChatMessagesProps {
-    messages: Content[];
-    isTyping: boolean;
-    messagesEndRef: React.RefObject<HTMLDivElement | null>;
+function getMessageRole(message: Message): MessageRole | undefined {
+  if (message instanceof TextMessage) {
+    return message.role;
+  } else if (message instanceof AgentStateMessage) {
+    return message.role;
+  } else if (message instanceof ImageMessage) {
+    return message.role;
+  }
+  return undefined;
 }
 
-export default function ChatMessages({ messages, isTyping, messagesEndRef }: ChatMessagesProps) {
+export default function ChatMessages({ messagesEndRef }: { messagesEndRef: RefObject<HTMLDivElement | null>}) {
+    
+        const {
+        visibleMessages,    // An array of messages that are currently visible in the chat.
+        appendMessage,      // A function to append a message to the chat.
+        setMessages,        // A function to set the messages in the chat.
+        deleteMessage,      // A function to delete a message from the chat.
+        reloadMessages,     // A function to reload the messages from the API.
+        stopGeneration,     // A function to stop the generation of the next message.
+        reset,              // A function to reset the chat.
+        isLoading,          // A boolean indicating if the chat is loading.
+    } = useCopilotChat();
+    
     return <>
         {
-            messages.map((message, i) => (
-                <div key={i} className={`message ${message.role}`}>
-                    {message.role === 'bot' && (
-                        <div className="bot-icon">                            
-                            <img src="gabe-bot.png" alt="Bot Icon" className="bot-icon" />
-                        </div>
-                    )}
-                    <div className={`message-content ${message.role}`}>
-                        {message.parts?.map((part,j) =>
-                            <Typography key={j} variant="body2" component="div">
-                                {/* Markdown expects a string or array of strings, not an array of objects or numbers */}
-                                {part.text &&
+            visibleMessages.map((message, i) => {
+
+                const role = getMessageRole(message)
+
+                return (
+                    <div key={i} className={`message ${role}`}>
+                        {role !== 'user' && (
+                            <div className="bot-icon">
+                                <img src="gabe-bot.png" alt="Bot Icon" className="bot-icon" />
+                            </div>
+                        )}
+
+                        {message.isTextMessage() &&
+                            <div className={`message-content ${role}`}>
+                            
+                                <Typography variant="body2" component="div">
                                     <Markdown remarkPlugins={[remarkGfm]}>
-                                        {part.text}
+                                        {(message as TextMessageInput).content}
                                     </Markdown>
-                                }
-                            </Typography>
-                        )}                        
+                                </Typography>
+                            
+                            </div>
+                        }
+
+                        {role === 'user' && (
+                            <div className="user-icon">
+                                <User className="icon" />
+                            </div>
+                        )}
                     </div>
-                    {message.role === 'user' && (
-                        <div className="user-icon">
-                            <User className="icon" />
-                        </div>
-                    )}
-                </div>
-            ))
+                )
+            })
         }
         {
-            isTyping && (
+            isLoading && (
                 <div className="typing-indicator">
                     <CircularProgress size={24} />
                 </div>
